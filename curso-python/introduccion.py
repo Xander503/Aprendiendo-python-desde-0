386c1a444486c26d24251c9948f39b68cf31ab78
AleXxRecinos1306 <00379423@uca.edu.sv>
2025-07-31 16:22:00 -0600

    primer commit

---
+++ curso-python/introduccion.py
@@ -0,0 +1,11 @@
+#Este es un comentario
+
+'''
+Este es un comentario de varias lineas
+Este es un comentario de varias lineas
+Este es un comentario de varias lineas
+Este es un comentario de varias lineas
+Este es un comentario de varias lineas
+'''
+
+print("hola mundo")
---
+++ curso-python/tipos-de-datos.py
@@ -0,0 +1,32 @@
+# Declaracion y asignacion de variables y tipos de datos
+
+#El tipo 'char' no existe en python en su lugar se utiliza un String (str)
+
+#string
+texto = str(35)
+texto2 = "46"
+
+#int
+entero = int(5)
+entero2 = 5
+
+#float
+decimal = float(6.2305)
+decimal2 = 5.3850
+
+#boolean
+booleano = True
+booleano2 = 5 > 4 
+
+#El tipo 'double' como tal no existe en python pero su equivalente es 'float'
+#decimal_preciso = double(2.97677964) esto no se puede hacer en python
+
+#Demostracion
+print(texto," ",type(texto))
+print(texto2," ",type(texto2))
+print(entero," ",type(entero))
+print(entero2," ",type(entero2))
+print(decimal," ",type(decimal))
+print(decimal2," ",type(decimal2))
+print(booleano," ",type(booleano))
+print(booleano2," ",type(booleano2))
